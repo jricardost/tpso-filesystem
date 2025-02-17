42bcbaf110ea10898ea59fc3d1fb5ad5b81a755f
Natan822 <natanmiguel821@gmail.com>
2025-02-17 11:39:18 -0300

    feat: inode searching function

--- Tools.java
+++ Tools.java
@@ -1,13 +1,46 @@
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.TimeZone;
 
 public final class Tools implements Constants {
-    
+
+    /*
+        Procura por um arquivo/diretório a partir do diretório atual dado um caminho
+        Retorna null se não encontrar
+    */
+    public static VirtualFileSystem.Inode findInode(String path) {
+        String[] splitPath = path.split("/");
+        return findInode(splitPath, Application.vfs.currentDirectory);
+    }
+
+    private static VirtualFileSystem.Inode findInode(String[] splitPath, VirtualFileSystem.Directory currentDir) {
+        List<VirtualFileSystem.Inode> inodes = new ArrayList<>(currentDir.files.values());
+
+        if (splitPath.length == 1) {
+            for (VirtualFileSystem.Inode inode : inodes) {
+                if (inode.name.equals(splitPath[0])) {
+                    return inode;
+                }
+            }
+            return null;
+        }
+
+        for (VirtualFileSystem.Inode inode : inodes) {
+            if (inode.name.equals(splitPath[0])) {
+                String[] newPath = Arrays.copyOfRange(splitPath, 1, splitPath.length);
+                return findInode(newPath, (VirtualFileSystem.Directory) inode);
+            }
+        }
+        return null;
+    }
+
     public static void clearScreen(){
         try {
             if (System.getProperty("os.name").toLowerCase().contains("win")) {

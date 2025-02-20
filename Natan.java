6bc5bfb5c546389a914b95abc6ce44c2feb8ceb8
Natan822 <natanmiguel821@gmail.com>
2025-02-20 13:44:36 -0300

    feat: tree command

--- Natan.java
+++ Natan.java
@@ -1,4 +1,11 @@
-public interface Natan {
+import java.awt.desktop.AppForegroundListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public interface Natan extends Constants {
+
+    static final int MAX_TREE_DEPTH = 1024;
 
     public static void cat(String ... args){
     }
@@ -8,8 +15,94 @@ public interface Natan {
     
     public static void grep(String ... args){
     }
-    
-    public static void tree(String ... args){
+
+    public static void tree(String ... args) {
+        String path = "";
+        if (args.length == 2) {
+            path = args[1];
+        }
+
+        Inode directory = VirtualFileSystem.read(path);
+        if (directory != null) {
+            String[] split = (Application.currentDirectory + "/" + path).split("/");
+            System.out.println(split[split.length - 1]);
+
+            printTree(directory, 0, true, new boolean[MAX_TREE_DEPTH]);
+        }
+    }
+    /*
+     * @param depth: Profundidade de diretórios atual(começa em 0)
+     *              Usado pra definir a identação da árvore
+     * @param isLastEntry: Indica se o node atual é o último de seu diretório
+     *                    Usado pra definir de vai ser usado o caracter "└" (true) ou "├" (false)
+     * @param dirsDepths: Array que controla onde os caracteres verticais "│" são exibidos.
+     *                   Cada índice representa uma profundidade na árvore:
+     *                   - Se 'dirsDepths[3]' for true, um "│" será desenhado na
+     *                     profundidade 3, indicando que há diretórios subsequentes
+     *                     nesse nível
+     *                   - Se 'dirsDepths[3]' for false, nenhum "│" será desenhado
+     *                     na profundidade 3, pois não há mais diretórios ativos nela
+     *   Profundidade:   0   1   2   3
+     *                   └───root
+     *                       ├───dir1
+     *                       │       file1
+     *                       │       file2
+     *                       └───dir2
+     *                               file1
+     */
+    private static void printTree(Inode node, int depth, boolean isLastEntry, boolean[] dirsDepths) {
+        if (node == null) return;
+
+        if (depth >= MAX_TREE_DEPTH) {
+            System.out.println("max tree depth of " + MAX_TREE_DEPTH + " reached");
+            return;
+        }
+
+        for (int i = 1; i < depth; i++) {
+            if (dirsDepths[i]) {
+                System.out.print("│   ");
+            }
+            else {
+                System.out.print("    ");
+            }
+        }
+
+        // Diretório
+        if (node.type == TYPE_DIRECTORY) {
+            if (depth != 0) {
+                if (isLastEntry) {
+                    System.out.print("└");
+                    dirsDepths[depth] = false;
+                }
+                else {
+                    System.out.print("├");
+                }
+                System.out.printf("───%s\n", node.name);
+            }
+            IDirectory dir = (IDirectory) node;
+            for(Inode inode : dir.files.values()) {
+                if (inode.type == TYPE_DIRECTORY && depth + 1 < MAX_TREE_DEPTH) {
+                    dirsDepths[depth + 1] = true;
+                    break;
+                }
+            }
+
+            List<Map.Entry<String, Inode>> entries = new ArrayList<>(dir.files.entrySet());
+            for (int i = 0; i < entries.size(); i++) {
+                Map.Entry<String, Inode> entry = entries.get(i);
+                printTree(entry.getValue(), depth + 1, (i == entries.size() - 1), dirsDepths);
+            }
+        }
+        // Arquivo
+        else {
+            if (dirsDepths[depth]) {
+                System.out.print("│   ");
+            }
+            else {
+                System.out.print("    ");
+            }
+            System.out.println(node.name);
+        }
     }
     
     public static void unzip(String ... args){

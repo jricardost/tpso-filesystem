5733b90956ee7d7294eff4ffb95726e7be6531a3
rodrigoacs <rodrigohths@gmail.com>
2025-02-20 21:07:09 -0300

    feat: cd, rename, rm

--- Mariana.java
+++ Mariana.java
@@ -46,6 +46,7 @@ public class Mariana {
 
 		Inode destination = vfs.read(destinationPath);
 		Inode source = vfs.read(sourcePath);
+
 		if (source == null) {
 			System.out.println("Source not found: " + sourcePath);
 			return;
@@ -56,31 +57,84 @@ public class Mariana {
 			return;
 		}
 
-		if (vfs.read(destinationPath.substring(0, destinationPath.lastIndexOf("/") - 1)) == null) {
+		if (vfs.read(destinationPath.substring(0, destinationPath.lastIndexOf("/"))) == null) {
 			System.out.println("Destination directory not found: " + destinationPath);
-
 		}
 
 		app.execute("touch", destinationPath);
 
-		// IFile sourceFile = (IFile) source;
-		// IFile newFile = new IFile(destinationPath);
-		// newFile.setContent(sourceFile.getContent()); // Copia aqui
-
-		// // Adiciona
-		// IDirectory currentDirectory = new IDirectory(app.currentDirectory);
-		// currentDirectory.files.put(destinationPath, newFile);
-
-		// System.out.println("File copied to " + destinationPath);
-
+		IFile sourceFile = (IFile) source;
+		IFile newFile = (IFile) vfs.read(destinationPath);
+		newFile.setContent(sourceFile.getContent());
 	}
 
 	public void cd(String... args) {
+		if (args.length != 2) {
+			System.out.println("Usage: cd <directory>");
+			return;
+		}
+
+		if (args.length == 1) {
+			app.currentDirectory = "/";
+			return;
+		}
+
+		if (args[1].equals("..")) {
+			app.currentDirectory = app.currentDirectory.substring(0, app.currentDirectory.lastIndexOf("/"));
+			return;
+		}
+
+		String directoryPath = args[1];
+		Inode directory = vfs.read(app.currentDirectory + "/" + directoryPath);
+
+		if (directory == null || !(directory instanceof IDirectory)) {
+			System.out.println("Directory not found: " + directoryPath);
+			return;
+		}
+
+		app.currentDirectory = app.currentDirectory + '/' + directoryPath;
 	}
 
 	public void rename(String... args) {
+		if (args.length != 3) {
+			System.out.println("Usage: rename <old_name> <new_name>");
+			return;
+		}
+
+		String oldName = args[1];
+		String newName = args[2];
+
+		IDirectory folder = (IDirectory) vfs.read(app.currentDirectory);
+		Inode inode = vfs.read(oldName);
+		if (inode != null) {
+			folder.files.remove(oldName);
+
+			inode.name = newName;
+			inode.absolutePath = inode.absolutePath.replace(oldName, newName);
+
+			folder.files.put(newName, inode);
+
+			System.out.println("Renamed " + oldName + " to " + newName);
+
+		} else {
+			System.out.println("File or directory not found: " + oldName);
+		}
 	}
 
 	public void rm(String... args) {
+		if (args.length != 2) {
+			System.out.println("Usage: rm <file>");
+			return;
+		}
+
+		String filePath = args[1];
+		Inode inode = vfs.read(filePath);
+
+		if (inode != null) {
+			IDirectory currentDirectory = (IDirectory) vfs.read(app.currentDirectory);
+			currentDirectory.files.remove(filePath);
+		} else {
+			System.out.println("File or directory not found: " + filePath);
+		}
 	}
 }

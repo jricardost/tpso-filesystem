0256cbf9d861abaf13623bb4845dc947b3ab4f4f
rodrigoacs <rodrigohths@gmail.com>
2025-02-21 00:49:30 -0300

    feat: unzip, tree, grep, du, cat!

--- Natan.java
+++ Natan.java
@@ -1,153 +1,173 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Natan {
-    
+
     Application app;
     UserAccountController uac;
     VirtualFileSystem vfs;
-    
-    public Natan(Application app, UserAccountController uac, VirtualFileSystem vfs){
+
+    static final int MAX_TREE_DEPTH = 1024;
+
+    public Natan(Application app, UserAccountController uac, VirtualFileSystem vfs) {
         this.app = app;
         this.uac = uac;
         this.vfs = vfs;
     }
-    
-    public static void cat(String ... args){
-        Inode inode = VirtualFileSystem.read(args[1]);
+
+    public void cat(String... args) {
+        Inode inode = vfs.read(args[1]);
         if (inode == null) {
             return;
         }
-        
-        if (inode.type == TYPE_DIRECTORY) {
+
+        if (inode.type == Constants.TYPE_DIRECTORY) {
             System.out.println("path specified is a directory");
             return;
         }
-        
+
         IFile file = (IFile) inode;
         System.out.println(String.join("\n", file.getContent()));
-        
     }
-    
-    public static void du(String ... args){
+
+    public void du(String... args) {
         String path = "";
         if (args.length == 2) {
             path = args[1];
         }
-        
-        Inode inode = VirtualFileSystem.read(path);
+
+        Inode inode = vfs.read(path);
         if (inode == null) {
             System.out.println("file or folder not found");
             return;
         }
         du(inode);
     }
-    
-    public static void grep(String ... args){
+
+    public void grep(String... args) {
         if (args.length != 3) {
             Tools.help(args[0]);
             return;
         }
-        
+
         String pattern = args[1];
         String path = args[2];
-        
-        Inode inode = VirtualFileSystem.read(path);
+
+        Inode inode = vfs.read(path);
         if (inode == null) {
             return;
         }
-        
-        if (inode.type == TYPE_DIRECTORY) {
+
+        if (inode.type == Constants.TYPE_DIRECTORY) {
             System.out.println("path specified is a directory");
             return;
         }
-        
+
         IFile file = (IFile) inode;
-        for(String line : file.getContent()) {
+        for (String line : file.getContent()) {
             if (line.contains(pattern)) {
                 System.out.println(line);
             }
         }
     }
-    
-    public static void tree(String ... args){
+
+    public void tree(String... args) {
         String path = "";
         if (args.length == 2) {
             path = args[1];
         }
-        
-        Inode directory = VirtualFileSystem.read(path);
-        if (directory != null && directory.type == TYPE_DIRECTORY) {
-            String[] split = (Application.currentDirectory + "/" + path).split("/");
+
+        Inode directory = vfs.read(path);
+        if (directory != null && directory.type == Constants.TYPE_DIRECTORY) {
+            String[] split = (app.currentDirectory(path)).split("/");
             System.out.println(split[split.length - 1]);
-            
+
             printTree(directory, 0, true, new boolean[MAX_TREE_DEPTH]);
         }
     }
-    
-    public static void unzip(String ... args){
+
+    public void unzip(String... args) {
         if (args.length == 1) {
             Tools.help(args[0]);
             return;
         }
-        
+
         String path = args[1];
-        Inode inode = VirtualFileSystem.read(path);
-        
-        if (inode == null) return;
-        
-        if (inode.type == TYPE_DIRECTORY) {
+        Inode inode = vfs.read(path);
+
+        if (inode == null)
+            return;
+
+        if (inode.type == Constants.TYPE_DIRECTORY) {
             System.out.println("path specified is a directory");
             return;
         }
-        
+
         if (!inode.name.endsWith(".zip")) {
             System.out.println("file is not zipped");
             return;
         }
-        
-        // Tira .zip do nome do arquivo
-        inode.name = inode.name.substring(0, inode.name.length() - 4);
-        inode.absolutePath = inode.absolutePath.substring(0, inode.absolutePath.length() - 4);
-        
-        // TODO: descomprimir conteúdo do arquivo se necessário(depende da implementação do comando 'zip')
-        
+
+        IFile file = (IFile) inode;
+        String[] content = file.getContent();
+        ArrayList<String> contentLines = new ArrayList<>();
+        for (String line : content) {
+            if (line.startsWith("f: ")) {
+                if (!contentLines.isEmpty()) {
+                    file.setContent(contentLines.toArray(new String[contentLines.size()]));
+                    contentLines.clear();
+                }
+                String[] split = line.split(" ");
+                String filename = split[1];
+                app.execute("touch", filename);
+                file = (IFile) vfs.read(filename);
+            } else {
+                contentLines.add(line);
+            }
+        }
+        if (!contentLines.isEmpty()) {
+            file.setContent(contentLines.toArray(new String[contentLines.size()]));
+        }
+
     }
-    
-    private static void printTree(Inode node, int depth, boolean isLastEntry, boolean[] dirsDepths) {
-        if (node == null) return;
-        
+
+    private void printTree(Inode node, int depth, boolean isLastEntry, boolean[] dirsDepths) {
+        if (node == null)
+            return;
+
         if (depth >= MAX_TREE_DEPTH) {
             System.out.println("max tree depth of " + MAX_TREE_DEPTH + " reached");
             return;
         }
-        
+
         for (int i = 1; i < depth; i++) {
             if (dirsDepths[i]) {
                 System.out.print("│   ");
-            }
-            else {
+            } else {
                 System.out.print("    ");
             }
         }
-        
+
         // Diretório
-        if (node.type == TYPE_DIRECTORY) {
+        if (node.type == Constants.TYPE_DIRECTORY) {
             if (depth != 0) {
                 if (isLastEntry) {
                     System.out.print("└");
                     dirsDepths[depth] = false;
-                }
-                else {
+                } else {
                     System.out.print("├");
                 }
                 System.out.printf("───%s\n", node.name);
             }
             IDirectory dir = (IDirectory) node;
-            for(Inode inode : dir.files.values()) {
-                if (inode.type == TYPE_DIRECTORY && depth + 1 < MAX_TREE_DEPTH) {
+            for (Inode inode : dir.files.values()) {
+                if (inode.type == Constants.TYPE_DIRECTORY && depth + 1 < MAX_TREE_DEPTH) {
                     dirsDepths[depth + 1] = true;
                     break;
                 }
             }
-            
+
             List<Map.Entry<String, Inode>> entries = new ArrayList<>(dir.files.entrySet());
             for (int i = 0; i < entries.size(); i++) {
                 Map.Entry<String, Inode> entry = entries.get(i);
@@ -158,23 +178,22 @@ public class Natan {
         else {
             if (dirsDepths[depth]) {
                 System.out.print("│   ");
-            }
-            else {
+            } else {
                 System.out.print("    ");
             }
             System.out.println(node.name);
         }
     }
 
-    private static void du(Inode rootNode) {
+    private void du(Inode rootNode) {
         // Arquivo
-        if (rootNode.type == TYPE_FILE) {
+        if (rootNode.type == Constants.TYPE_FILE) {
             System.out.printf("%-8d %s%n", rootNode.size, rootNode.absolutePath);
             return;
         }
         // Diretório
         IDirectory directory = (IDirectory) rootNode;
-        for(Inode inode : directory.files.values()) {
+        for (Inode inode : directory.files.values()) {
             du(inode);
         }
     }
